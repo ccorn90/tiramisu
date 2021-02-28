02ff943418ffb1daf3d3cec8f35f17747ec17adb
ccorn90 <ccorn90@gmail.com>
2021-02-27 22:57:00 -0800

    read and echo files

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import System.IO
+import System.Environment
 import Lib
 
+readf fname = do
+    handle <- openFile fname ReadMode
+    hGetContents handle
+
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    contents <- mapM readf args
+    putStrLn (foldl1 (++) contents)
+
